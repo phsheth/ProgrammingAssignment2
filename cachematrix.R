d828f529392b64b02d58d3929e152f95b9fb435c
Pushkar Sheth <pushkar@briskfyr.com>
2017-09-08 14:29:38 +0530

    corrected submission
    Made corrections like
    1. Comment for each function
    2. checking if matrix is square. End loop if matrix not square.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,12 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+#The first function, makeCacheMatrix creates a special "vector", which is really a list containing a function to
+
+#set the value of the matrix
+#get the value of the matrix - Also checks if the matrix is square - if not - it breaks the function execution in cacheSolve
+#set the value of the inverse of matrix
+#get the value of the inverse of matrix
 
 makeCacheMatrix <- function(x = matrix()) {
   matinv <- NULL
@@ -9,7 +14,20 @@ makeCacheMatrix <- function(x = matrix()) {
     x <<- y
     matinv <<- NULL
   }
-  get <- function() x
+  get <- function() {
+    if((!(nrow(x) == ncol(x))) ==TRUE) {
+      message("Matrix is not square")
+      break
+    } else {
+      x
+    }
+    
+    
+    
+    
+  }
+  
+  
   setinv <- function(solve) matinv <<- solve
   getinv <- function() matinv
   list(setinv = set, get = get,
@@ -18,15 +36,16 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+#The following function calculates the inverse of the special "vector" created with the above function. However, it first checks to see if the inverse of matrix has already been calculated. If so, it gets the inverse of matrix from the cache and skips the computation. Otherwise, it calculates the inverse of the data and sets the value of the inverse in the cache via the setinv function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
   matinv <- x$getinv()
   if(!is.null(matinv)) {
     message("getting cached data")
     return(matinv)
   }
+
   data <- x$get()
   matinv <- solve(data, ...)
   x$setinv(matinv)
